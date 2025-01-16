63f68a7e48b32b889b4b95f362de44414b7e4557
SeukWon Kang <kasworld@gmail.com>
2025-01-16 16:26:58 +0900

    fix warning, 단추 연결

--- 다각기둥.gd
+++ 다각기둥.gd
@@ -3,16 +3,16 @@ extends MeshInstance3D
 class_name 다각기둥
 
 func init(반지름 :float, 높이 :float, 색깔 :Color, 각수 :int = 64) -> void:
-	var mesh = CylinderMesh.new()
-	mesh.bottom_radius = 반지름
-	mesh.top_radius = 반지름
-	mesh.height = 높이
-	mesh.radial_segments = 각수
-	#mesh.rings = 100
+	var msh = CylinderMesh.new()
+	msh.bottom_radius = 반지름
+	msh.top_radius = 반지름
+	msh.height = 높이
+	msh.radial_segments = 각수
+	#msh.rings = 100
 	var mat = StandardMaterial3D.new()
 	mat.albedo_color = 색깔
-	mesh.material = mat
-	$".".mesh = mesh
+	msh.material = mat
+	$".".mesh = msh
 	$".".rotate_x(PI/2)
 
 func set_color(co :Color) -> void:
