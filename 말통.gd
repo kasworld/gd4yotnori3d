adfc29095f2ede2f90a37121c6dd32924828c3a6
SeukWon Kang <kasworld@gmail.com>
2025-01-18 16:26:27 +0900

    말통 넣을공간 설정 개선

--- 말통.gd
+++ 말통.gd
@@ -4,19 +4,21 @@ class_name 말통
 
 var 반지름 :float
 var 높이 :float
-var 넣을크기비율 :float = 1.0
-var 높이범위 :Vector2
+var 넣을반경크기 :float
+var 넣을높이범위 :Vector2
 
-func init(반지름a :float, 높이a :float, 색깔 :Color, 각수 := 64, 넣을크기비율a := 1.0, 높이범위a :=Vector2(0.5,1.0) ) -> 말통:
+func init(반지름a :float, 높이a :float, 색깔 :Color, 각수 := 64, 넣을크기반지름비율 := 1.0, 넣을높이범위비율 :=Vector2(0.5,1.0) ) -> 말통:
 	반지름 = 반지름a
 	높이 = 높이a
 	$"모양".init(반지름, 높이, 색깔, 각수)
-	넣을공간설정(넣을크기비율a,높이범위a)
+	넣을공간설정(반지름*넣을크기반지름비율,높이*넣을높이범위비율)
 	return self
 
-func 넣을공간설정(넣을크기비율a :float, 높이범위a :Vector2) -> void:
-	넣을크기비율 = 넣을크기비율a
-	높이범위 = 높이범위a
+# 인자가 비율이 아닌 크기임.
+func 넣을공간설정(넣을반경크기a :float, 넣을높이범위a :Vector2) -> 말통:
+	넣을반경크기 = 넣을반경크기a
+	넣을높이범위 = 넣을높이범위a
+	return self
 
 func 설명달기(설명 :String, 위치 := Vector3.ZERO, 설명색깔 := Color.WHITE) -> 말통:
 	$"설명".text = 설명
@@ -33,7 +35,7 @@ func 말들넣기(넣을말들 :Array[말]) -> void:
 
 func 말넣기(넣을말 :말) -> void:
 	$"말들".add_child(넣을말)
-	넣을말.position = rand_circle(반지름*넣을크기비율, 높이범위*높이)
+	넣을말.position = rand_circle(넣을반경크기, 넣을높이범위)
 
 func 말모두빼기() -> Array[말]:
 	var rtn :Array[말]
