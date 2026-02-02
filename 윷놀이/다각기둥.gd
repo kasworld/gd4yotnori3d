963806001668d81cccf5061e630207841b9e536f
seukwonkang <kasworld@gmail.com>
2026-02-02 16:13:10 +0900

    update 다각기둥

--- 윷놀이/다각기둥.gd
+++ 윷놀이/다각기둥.gd
@@ -3,18 +3,15 @@ extends MeshInstance3D
 class_name 다각기둥
 
 func init(반지름 :float, 높이 :float, 색깔 :Color, 각수 :int = 64) -> 다각기둥:
-	var msh = CylinderMesh.new()
-	msh.bottom_radius = 반지름
-	msh.top_radius = 반지름
-	msh.height = 높이
-	msh.radial_segments = 각수
-	#msh.rings = 100
-	var mat = StandardMaterial3D.new()
-	mat.albedo_color = 색깔
-	msh.material = mat
-	$".".mesh = msh
-	$".".rotation.x = PI/2
+	mesh = CylinderMesh.new()
+	mesh.bottom_radius = 반지름
+	mesh.top_radius = 반지름
+	mesh.height = 높이
+	mesh.radial_segments = 각수
+	mesh.material = MultiMeshShape.make_color_material()
+	mesh.material.albedo_color = 색깔
+	rotation.x = PI/2
 	return self
 
 func set_color(co :Color) -> void:
-	$".".mesh.material.albedo_color = co
+	mesh.material.albedo_color = co
